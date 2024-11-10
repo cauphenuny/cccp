fd808622c42af964fadc741b289c76d086b94812
cauphenuny <starseekist@gmail.com>
2024-11-10 12:37:45 +0800

    implement if/while statement for brainf**k backend

--- src/target/bf.cpp
+++ src/target/bf.cpp
@@ -3,16 +3,29 @@
 
 #include <map>
 #include <string>
+#include <vector>
 
 const int MAXN = 65536;
 
 struct BaseIR::BfContext {
     struct Tape {
         unsigned current_pos{0};
-        bool used[MAXN]{0};
+        bool cur_used[MAXN]{0};
+        bool non_zero[MAXN]{0};
     };
     Tape tape;
     std::map<std::string, int> symbol_table;
+    unsigned pc, global_ret;
+    std::map<std::string, int> labels;
+    int getBlockPC(const std::string& block_name) {
+        if (!labels.contains(block_name)) {
+            int pc = labels.size() + 1;  // reserve pc=0 for skip
+            debugLog("alloc pc @{} for block `{}`", pc, block_name.length() ? block_name : "entry");
+            labels[block_name] = pc;
+        }
+        return labels[block_name];
+    }
+    std::function<std::string()> label_end = []() { return ""; };
     unsigned ret;
 };
 
@@ -42,8 +55,11 @@ std::string bfCompress(std::string s) {
 }
 
 static void bfFree(Tape& tape, unsigned pos, unsigned size = 1) {
-    for (unsigned i = 0; i < size; i++)
-        assert(tape.used[pos + i] && "free unallocated cell!"), tape.used[pos + i] = false;
+    for (unsigned i = 0; i < size; i++) {
+        // assert(tape.cur_used[pos + i] && "double free!");
+        if (!tape.cur_used[pos + i]) debugLog("warning: double freed cell #{}", pos + i);
+        tape.cur_used[pos + i] = false;
+    }
 }
 
 /// @brief move cell pointer to the given position
@@ -56,73 +72,109 @@ static std::string bfJump(Tape& tape, unsigned pos) {
     else if (tape.current_pos > pos)
         str = repeat("<", tape.current_pos - pos);
     tape.current_pos = pos;
-    return str + "    \t" + comment;
+    return str + "  \t" + comment;
 }
 
 /// @brief do {func} for @{pos} times, take the ownership of cell ${pos}
-static std::string bfFor(Tape& tape, unsigned pos, std::function<std::string()> func) {
-    assert(tape.used[pos] && "bad access");
+static std::string bfFor(Tape& tape, unsigned pos, std::function<std::string()> func,
+                         bool free_after_move = true) {
+    assert(tape.cur_used[pos] && "bad access");
     const std::string comment = std::format("; do for ${} times\n", pos);
     std::string str;
     str += bfJump(tape, pos);
     str += "[\n";
-    str += addIndent(func() + bfJump(tape, pos));
-    str += "-]\n";
-    bfFree(tape, pos);
+    str += addIndent(func());
+    str += bfJump(tape, pos) + "-]\n";
+    if (free_after_move) bfFree(tape, pos);
+    // tape.non_zero[pos] = false;
     return comment + addIndent(str);
 }
-
-// static std::string bfIf(Tape& tape, unsigned pos, std::function<std::string()> func) {
-//     assert(tape.used[pos] && "bad access");
-//     const std::string comment = std::format("; if ${}\n", pos);
-//     std::string str;
-//     str += bfJump(tape, pos);
-//     str += "[\n";
-//     str += addIndent(func());
-//     str += bfJump(tape, pos);
-//     str += "]\n";
-//     bfFree(tape, pos);
-//     return comment + addIndent(str);
-// }
 
 /// @brief clear cell ${pos:size}
 static std::string bfClear(Tape& tape, unsigned pos, unsigned size = 1) {
     const std::string comment = std::format("; clear(#{} {})\n", pos, size);
     std::string str;
-    for (unsigned i = 0; i < size; i++)
-        assert(tape.used[pos + i] && "bad access"), str += bfJump(tape, pos + i) + "[-]\n";
-    return comment + addIndent(str);
+    for (unsigned i = 0; i < size; i++) {
+        if (!tape.non_zero[pos + i]) continue;
+        assert(tape.cur_used[pos + i] && "bad access"), str += bfJump(tape, pos + i) + "[-]\n";
+    }
+    if (str.length())
+        return comment + addIndent(str);
+    else
+        return "";
 }
 
 /// @brief allocate a segment cell, length: {size}, returned first cell: ${pos}
 static std::string bfAlloc(Tape& tape, unsigned& pos, unsigned size = 1) {
     pos = tape.current_pos;
     while (1) {
-        if (std::all_of(tape.used + pos, tape.used + pos + size, [](bool used) { return !used; })) {
+        if (std::all_of(tape.cur_used + pos, tape.cur_used + pos + size,
+                        [](bool used) { return !used; })) {
             break;
         }
         pos++;
     }
-    for (unsigned i = 0; i < size; i++) tape.used[pos + i] = true;
-    return bfClear(tape, pos, size);
+    for (unsigned i = 0; i < size; i++) tape.cur_used[pos + i] = true;
+    std::string str = bfClear(tape, pos, size);
+    for (unsigned i = 0; i < size; i++) tape.non_zero[pos + i] = true;
+    // return std::format("; alloc #{} ~ #{}\n", pos, pos + size - 1) + addIndent(str);
+    return str;
+}
+
+static std::string bfIf(Tape& tape, unsigned pos, std::function<std::string()> func) {
+    assert(tape.cur_used[pos] && "bad access");
+    const std::string comment = std::format("; if ${}\n", pos);
+    std::string str;
+    str += bfJump(tape, pos);
+    str += "[[-]\n";
+    str += addIndent(func());
+    str += bfJump(tape, pos);
+    str += "]\n";
+    bfFree(tape, pos);
+    return comment + addIndent(str);
+}
+
+static std::string bfIfElse(Tape& tape, unsigned pos, std::function<std::string()> then_func,
+                            std::function<std::string()> else_func) {
+    assert(tape.cur_used[pos] && "bad access");
+    const std::string comment1 = std::format("; if ${}\n", pos), comment2 = std::format("; else");
+    std::string str1, str2;
+    unsigned tmp;
+    str1 += bfAlloc(tape, tmp);
+    str1 += bfJump(tape, tmp) + "+\n";
+    str1 += bfJump(tape, pos);
+    str1 += "[\n[-]\n";
+    str1 += bfJump(tape, tmp) + "-\n";
+    str1 += addIndent(then_func());
+    str1 += bfJump(tape, pos);
+    str1 += "]\n";
+    str2 += bfJump(tape, tmp);
+    str2 += "[-\n";
+    str2 += addIndent(else_func());
+    str2 += bfJump(tape, tmp);
+    str2 += "]\n";
+    bfFree(tape, tmp);
+    bfFree(tape, pos);
+    return comment1 + addIndent(str1) + comment2 + addIndent(str2);
 }
 
 /// @brief move cell from src to dest, take the ownership of ${src}
-static std::string bfMove(Tape& tape, unsigned src, unsigned dest, int sign = 1) {
-    assert(tape.used[src] && "bad access");
-    assert(tape.used[dest] && "bad access");
+static std::string bfMove(Tape& tape, unsigned src, unsigned dest, int sign = 1,
+                          bool free_after_move = true) {
+    assert(tape.cur_used[src] && "bad access");
+    assert(tape.cur_used[dest] && "bad access");
     if (src == dest) return "";
-    const std::string comment = std::format("; move(#{} to #{})\n", src, dest);
+    const std::string comment = std::format("; move(#{}) to #{}\n", src, dest);
     std::string str, ch = sign > 0 ? "+" : "-";
-    str += bfFor(tape, src, [&] { return bfJump(tape, dest) + ch + "\n"; });
+    str += bfFor(tape, src, [&] { return bfJump(tape, dest) + ch + "\n"; }, free_after_move);
     return comment + addIndent(str);
 }
 
 static std::string bfCopy(Tape& tape, unsigned src, std::vector<unsigned> dest, int sign) {
-    std::string comment = std::format("; copy(#{} to ", src);
+    std::string comment = std::format("; copy(#{}) to ", src);
     for (auto d : dest) {
         comment += std::format("#{} ", d);
-        assert(tape.used[d] && "bad access");
+        assert(tape.cur_used[d] && "bad access");
     }
     comment += "\n";
     std::string str, ch = sign == 1 ? "+" : "-";
@@ -138,7 +190,7 @@ static std::string bfCopy(Tape& tape, unsigned src, std::vector<unsigned> dest, 
         s += bfJump(tape, src);
         return s;
     });
-    tape.used[src] = true;
+    tape.cur_used[src] = true;
     str += bfMove(tape, tmp, src);
     return comment + addIndent(str);
 }
@@ -152,7 +204,7 @@ static std::string bfCopy(Tape& tape, unsigned src, unsigned dest, int sign = 1,
 }
 
 static std::string bfInc(Tape& tape, int pos, int val) {
-    assert(tape.used[pos] && "bad access");
+    assert(tape.cur_used[pos] && "bad access");
     std::string str, ch, name;
     if (val > 0)
         ch = "+", name = "inc";
@@ -165,7 +217,7 @@ static std::string bfInc(Tape& tape, int pos, int val) {
 }
 
 static std::string bfAdd(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; add(${} ${}) to #{}\n", x, y, ret);
     std::string str;
     str += bfMove(tape, x, ret, 1);
@@ -174,7 +226,7 @@ static std::string bfAdd(Tape& tape, unsigned x, unsigned y, unsigned ret) {
 }
 
 static std::string bfSub(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; sub(${} ${}) to #{}\n", x, y, ret);
     std::string str = bfMove(tape, x, ret, 1);
     str += bfMove(tape, y, ret, -1);
@@ -182,7 +234,7 @@ static std::string bfSub(Tape& tape, unsigned x, unsigned y, unsigned ret) {
 }
 
 static std::string bfMul(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; mul(${} ${}) to #{}\n", x, y, ret);
     std::string str;
     str += bfFor(tape, x, [&] { return bfCopy(tape, y, ret, 1); });
@@ -191,7 +243,7 @@ static std::string bfMul(Tape& tape, unsigned x, unsigned y, unsigned ret) {
 }
 
 static std::string bfDiv(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; div(${} ${}) to #{}\n", x, y, ret);
     debugLog("{}", comment);
     std::string str;
@@ -232,7 +284,7 @@ static std::string bfDiv(Tape& tape, unsigned x, unsigned y, unsigned ret) {
 // }
 
 static std::string bfOr(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; or(${} ${}) to #{}\n", x, y, ret);
     std::string str;
     str += bfJump(tape, x);
@@ -242,7 +294,7 @@ static std::string bfOr(Tape& tape, unsigned x, unsigned y, unsigned ret) {
 }
 
 static std::string bfEqual(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; eq(${} ${}) to #{}\n", x, y, ret);
     std::string str;
     str += bfJump(tape, x);
@@ -253,9 +305,9 @@ static std::string bfEqual(Tape& tape, unsigned x, unsigned y, unsigned ret) {
     return comment + addIndent(str);
 }
 
-static std::string bfNot(Tape& tape, unsigned x, unsigned ret) {
-    assert(tape.used[x] && tape.used[ret] && "bad access");
-    const std::string comment = std::format("; not(${}) to #{}\n", x, ret);
+static std::string bfBitNot(Tape& tape, unsigned x, unsigned ret) {
+    assert(tape.cur_used[x] && tape.cur_used[ret] && "bad access");
+    const std::string comment = std::format("; bit not(${}) to #{}\n", x, ret);
     unsigned tmp;
     std::string str;
     str += bfAlloc(tape, tmp) + bfJump(tape, x) + "-\n[\n" + bfJump(tape, tmp) + "-\n" +
@@ -266,14 +318,23 @@ static std::string bfNot(Tape& tape, unsigned x, unsigned ret) {
     return comment + addIndent(str);
 }
 
+static std::string bfLogicNot(Tape& tape, unsigned x, unsigned ret) {
+    assert(tape.cur_used[x] && tape.cur_used[ret] && "bad access");
+    const std::string comment = std::format("; not(${}) to #{}\n", x, ret);
+    std::string str;
+    str += bfInc(tape, ret, 1);
+    str += bfIf(tape, x, [&] { return bfInc(tape, ret, -1); });
+    return comment + addIndent(str);
+}
+
 static std::string bfNeq(Tape& tape, unsigned x, unsigned y, unsigned ret) {
-    assert(tape.used[x] && tape.used[y] && tape.used[ret] && "bad access");
+    assert(tape.cur_used[x] && tape.cur_used[y] && tape.cur_used[ret] && "bad access");
     const std::string comment = std::format("; neq(${} ${}) to #{}\n", x, y, ret);
     std::string str;
     unsigned tmp;
     str += bfAlloc(tape, tmp);
     str += bfEqual(tape, x, y, tmp);
-    str += bfNot(tape, tmp, ret);
+    str += bfLogicNot(tape, tmp, ret);
     return comment + addIndent(str);
 }
 
@@ -323,17 +384,19 @@ static BfBinaryFunction getBfFunction(Operator oper) {
 }
 
 std::string ValueIR::printBf(bool compress, std::shared_ptr<BfContext> context) const {
-    std::string str;
+    std::string str, comment;
     assert(context != nullptr);
     auto& ctx = *context;
     switch (inst) {
+        case Inst::String: ctx.ret = ctx.getBlockPC(content); break;
         case Inst::Return:
             str += params[0]->printBf(compress, context);
-            str += bfJump(ctx.tape, ctx.ret);
+            comment = std::format("; return ${}\n", ctx.ret);
+            str += bfClear(ctx.tape, ctx.global_ret);
+            str += bfMove(ctx.tape, ctx.ret, ctx.global_ret);
+            str += bfClear(ctx.tape, ctx.pc);
             break;
-        case Inst::Integer:
-            str += bfInteger(ctx.tape, ctx.ret, std::stoi(content));
-            break;
+        case Inst::Integer: str += bfInteger(ctx.tape, ctx.ret, std::stoi(content)); break;
         case Inst::Binary: {
             str += params[0]->printBf(compress, context);
             const unsigned op1 = ctx.ret;
@@ -344,9 +407,12 @@ std::string ValueIR::printBf(bool compress, std::shared_ptr<BfContext> context) 
             break;
         }
         case Inst::Alloc:
-            str += bfAlloc(ctx.tape, ctx.ret);
-            str += std::format("; alloc #{} for @{}\n", ctx.ret, content);
-            ctx.symbol_table[content] = ctx.ret;
+            if (!ctx.symbol_table.contains(content))
+                throw runtimeError("unallocated variable {}", content);
+            // str += bfAlloc(ctx.tape, ctx.ret);
+            // str += std::format("; alloc #{} for {}\n", ctx.ret, content);
+            // debugLog("alloc #{} for variable {}", ctx.ret, content);
+            // ctx.symbol_table[content] = ctx.ret;
             break;
         case Inst::Store: {
             str += params[0]->printBf(compress, context);
@@ -358,11 +424,54 @@ std::string ValueIR::printBf(bool compress, std::shared_ptr<BfContext> context) 
         }
         case Inst::Load:
             str += bfAlloc(ctx.tape, ctx.ret);
-            str += std::format("; load @{} to #{}\n", content, ctx.ret);
+            str += std::format("; load {} to #{}\n", content, ctx.ret);
             str += bfCopy(ctx.tape, ctx.symbol_table[content], ctx.ret);
             break;
-        default: throw runtimeError("unimplemented value type {}!", inst); break;
+        case Inst::Jump: {
+            unsigned target = ctx.getBlockPC(content);
+            comment = std::format("; jump {}(pc: {}):\n", content, target);
+            str += bfJump(ctx.tape, ctx.pc);
+            str += "[-]" + repeat("+", target) + "\n";
+            break;
+        }
+        case Inst::Branch: {
+            str += params[0]->printBf(compress, context);
+            unsigned exp = ctx.ret;
+            params[1]->printBf(compress, context);
+            unsigned block_pc1 = ctx.ret;
+            params[2]->printBf(compress, context);
+            unsigned block_pc2 = ctx.ret;
+            str += bfIfElse(
+                ctx.tape, exp,
+                [&] { return bfJump(ctx.tape, ctx.pc) + "[-]" + repeat("+", block_pc1) + "\n"; },
+                [&] { return bfJump(ctx.tape, ctx.pc) + "[-]" + repeat("+", block_pc2) + "\n"; });
+            break;
+        }
+        case Inst::Label: {
+            str += ctx.label_end();
+            int pc = ctx.getBlockPC(content);
+            comment =
+                std::format("; block {}(pc: {}):\n", content.length() ? content : "entry", pc);
+            unsigned tmp, tmp2;
+            str += bfJump(ctx.tape, ctx.pc);
+            str += bfAlloc(ctx.tape, tmp, 2);
+            str += bfCopy(ctx.tape, ctx.pc, tmp);
+            str += bfInteger(ctx.tape, tmp2, pc);
+            str += bfEqual(ctx.tape, tmp, tmp2, tmp + 1);
+            str += bfJump(ctx.tape, tmp + 1);
+            str += "[-\n";
+            ctx.label_end = [tmp, this, context]() {
+                bfFree(context->tape, tmp + 1);
+                return std::format("; end block {}: jump to entry #{}\n",
+                                   this->content.length() ? this->content : "entry", tmp + 1) +
+                       bfJump(context->tape, tmp + 1) + "]\n";
+            };
+            break;
+        }
+        default: throw runtimeError("unimplemented value type `{}`", inst); break;
     }
+    if (comment.length()) str = comment + addIndent(str);
+    if (inst != Inst::Label) return addIndent(str);
     return str;
 }
 
@@ -375,12 +484,64 @@ std::string MultiValueIR::printBf(bool compress, std::shared_ptr<BfContext> cont
 }
 
 std::string FunctionIR::printBf(bool compress, std::shared_ptr<BfContext> context) const {
-    std::string blocks_str;
-    // for (const auto& block : blocks) {
-    blocks_str += blocks->printBf(compress, context);
-    // }
-    // std::cerr << compress(blocks_str) << std::endl;
-    return "; " + name + "()\n" + addIndent(blocks_str);
+    std::string str;
+    context = std::make_shared<BfContext>();
+
+    str += bfAlloc(context->tape, context->global_ret);
+    context->symbol_table["$RET"] = context->global_ret;
+    str += bfAlloc(context->tape, context->pc);
+    context->symbol_table["$PC"] = context->pc;
+    debugLog("RET: #{}, PC: #{}", context->ret, context->pc);
+
+    auto alloc = [&](ValueIR* value) {
+        if (value->inst == Inst::Alloc) {
+            unsigned pos;
+            str += bfAlloc(context->tape, pos);
+            context->symbol_table[value->content] = pos;
+            debugLog("alloc cell #{} for variable `{}`", pos, value->content);
+        }
+    };
+    std::function<void(BaseIR*)> traverse = [&](BaseIR* base) {
+        if (MultiValueIR* multi = dynamic_cast<MultiValueIR*>(base)) {
+            for (auto& value : multi->values) {
+                traverse(value.get());
+            }
+        } else if (ValueIR* value = dynamic_cast<ValueIR*>(base)) {
+            alloc(value);
+            for (auto& param : value->params) {
+                traverse(param.get());
+            }
+        }
+    };
+    traverse(blocks.get());
+
+    str += bfInc(context->tape, context->pc, context->getBlockPC(""));
+    // str += bfInc(context->tape, context->global_ret, 3);
+    str += bfJump(context->tape, context->pc);
+    str += "[  \t; pc\n";
+
+    str += addIndent(blocks->printBf(compress, context));
+    str += addIndent(context->label_end());
+
+    str += bfJump(context->tape, context->pc);
+    str += "]  \t; pc\n";
+    str += bfJump(context->tape, context->global_ret);
+
+    std::vector<unsigned> pos;
+    for (unsigned i = 2; i < MAXN; i++) {
+        if (context->tape.cur_used[i]) {
+            int flag = 1;
+            for (auto& [name, p] : context->symbol_table) {
+                if (p == i) flag = 0;
+            }
+            if (flag) pos.push_back(i);
+        }
+    }
+    if (pos.size()) {
+        debugLog("warning: memory leaked at {}", pos);
+    }
+
+    return "; " + name + "()\n" + addIndent(str);
 }
 
 std::string ProgramIR::printBf(bool compress, std::shared_ptr<BfContext> context) const {
